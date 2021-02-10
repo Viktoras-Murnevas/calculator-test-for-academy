287c3fd0de515221123eddf2db49205c48b9569e
Viktoras Murnevas <darksfinx@hotmail.com>
2021-02-10 23:04:33 +0200

    First commit

--- src/test/java/clean/entities/CalculationOperationsTest.java
+++ src/test/java/clean/entities/CalculationOperationsTest.java
@@ -13,4 +13,26 @@ public class CalculationOperationsTest {
 
         assertEquals(result, new BigDecimal("25"));
     }
+
+    @Test
+    public void returnsMinus_whenSubstractionSelected() {
+        BigDecimal result = CalculationOperations.SUBTRACTION.calculate(new BigDecimal("10"), new BigDecimal("5"));
+
+        assertEquals(result, new BigDecimal("5"));
+    }
+
+    @Test
+    public void returnsmultiplication_whenMultiplicationSelected() {
+        BigDecimal result = CalculationOperations.MULTIPLICATION.calculate(new BigDecimal("10"), new BigDecimal("5"));
+
+        assertEquals(result, new BigDecimal("50"));
+    }
+
+    @Test
+    public void returnsDIVISION_whenDivisionSelected() {
+        BigDecimal result = CalculationOperations.DIVISION.calculate(new BigDecimal("10"), new BigDecimal("5"));
+
+        assertEquals(result, new BigDecimal("2"));
+    }
+
 }
--- src/test/java/clean/operationconstruction/OperationConstructorTest.java
+++ src/test/java/clean/operationconstruction/OperationConstructorTest.java
@@ -1,7 +1,6 @@
 package clean.operationconstruction;
 
-import static org.mockito.Mockito.when;
-
+import clean.entities.CalculationStatement;
 import clean.exceptions.InvalidInformationEnteredException;
 import clean.operationconstrution.OperationConstructor;
 import clean.userinteraction.UserInteractor;
@@ -12,6 +11,9 @@ import org.junit.rules.ExpectedException;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class OperationConstructorTest {
 
@@ -33,8 +35,31 @@ public class OperationConstructorTest {
     public void fails_whenInvalidOperationProvided() throws Exception {
         expectedException.expect(InvalidInformationEnteredException.class);
         expectedException.expectMessage("Wrong operation");
+//        when(userInteractor.writeAndGetResponse("Enter operation")).thenReturn("Not valid operation");
+        when(userInteractor.writeAndGetResponse("Enter operation")).thenReturn("8"); //thenReturn("-")
 
-        when(userInteractor.writeAndGetResponse("Enter operation")).thenReturn("Not valid operation");
+        operationConstructor.fetchCalculationDetails();
+    }
+
+    @Test
+    public void fails_whenInvalidFirstOperandProvided() throws Exception {
+        expectedException.expect(InvalidInformationEnteredException.class);
+        expectedException.expectMessage("Wrong first operand");
+
+        when(userInteractor.writeAndGetResponse("Enter operation")).thenReturn("SUBTRACTION");
+        when(userInteractor.writeAndGetResponse("Enter first operand")).thenReturn("m8");
+
+        operationConstructor.fetchCalculationDetails();
+    }
+
+    @Test
+    public void fails_whenInvalidSecondOperandProvided() throws Exception {
+        expectedException.expect(InvalidInformationEnteredException.class);
+        expectedException.expectMessage("Wrong second operand");
+
+        when(userInteractor.writeAndGetResponse("Enter operation")).thenReturn("SUBTRACTION");
+        when(userInteractor.writeAndGetResponse("Enter first operand")).thenReturn("8");
+        when(userInteractor.writeAndGetResponse("Enter second operand")).thenReturn("m8");
 
         operationConstructor.fetchCalculationDetails();
     }
